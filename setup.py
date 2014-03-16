e7ffc14a41496325e75cc2880ecdce9985f2bafb
John Novak <john@johnnovak.net>
2014-03-16 18:39:23 +1000

    setup.py fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(name='twyg',
       version='0.1',
       description='Scriptable tree visualisatino for Python',
-      long_description=open('README.rst').read(),
+      long_description=open('README.md').read(),
 
       classifiers=[
         'Development Status :: 3 - Alpha',
