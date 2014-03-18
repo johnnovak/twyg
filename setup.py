3c7f6488b55e5b3c6166ce55e62eb5781a2f97cf
John Novak <john@johnnovak.net>
2014-03-19 09:27:00 +1000

    setup.py complete

--- setup.py
+++ setup.py
@@ -7,15 +7,33 @@ setup(name='twyg',
       description='Generative tree visualiser for Python',
       long_description=open('README.md').read(),
 
-      # TODO
       classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+
+        'Environment :: Console',
+        'Environment :: Other Environment',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Topic :: Graphics :: Visualisation'
+
+        'Topic :: Artistic Software',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Libraries :: Python Modules'
       ],
 
-      keywords='tree graph visualisation generative graphics layout nodebox',
+      keywords='tree graph visualisation generative graphics layout
+      nodebox cairo pdf png postscript svg',
       url='http://www.johnnovak.net/twyg/',
       author='John Novak',
       author_email='john@johnnovak.net',
@@ -24,8 +42,7 @@ setup(name='twyg',
       packages=['twyg'],
       include_package_data=True,
 
-      # TODO
-      install_requires=[],
+      install_requires=['cairo'],
 
       entry_points = {
         'console_scripts': ['twyg=twyg.cmdline:main']
